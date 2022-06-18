f9aaddb121554c2302f8e301aa5fbce33548338a
Harshvardhan Joshi <48888309+harshjoshi23@users.noreply.github.com>
2022-06-18 23:29:38 +0530

    adding inline JSXCSS

--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
 import React from "react";
 import  ReactDOM  from "react-dom"
 
+//CSS
+import './index.css'
+
 function Booklist(){
-  return( <section><Book/></section> )
+  return( <section className="booklist">
+  <Book/>
+  <Book/>
+  <Book/>
+  <Book/></section> )
 }
 
 const Book = () => {
-  return(<article>
-    <Image></Image>
+  return(<article className="book">
+    <Image/>
     <Title/>
     <Authr/>
   </article>)
 }
-const Image = () => {return(<img src="https://images-eu.ssl-images-amazon.com/images
-/I/71g2ednj0JL._AC_UL906_SR906,600_.jpg" alt=""/>)}
-const Title = () => {return(<h1>The Psychology of Money</h1>)}
-const Authr = () => {return(<h4>Morgan Housel</h4>)}
+const Image = () => {return(<img src="https://m.media-amazon.com/images/I/81bsw6fnUiL
+._AC_UL480_QL65_.jpg" alt=""/>)}
+const Title = () => {return(<h1>Rich Dad Poor Dad: What the Rich Teach Their Kids
+   About Money That the Poor and Middle Class Do Not!</h1>)}
+
+// Using inline CSS in the React
+const Authr = () => {return(<h4 style={{
+  color:'#617d98',fontSize:'0.75rem',marginTop:'0.25rem'
+}}>Robert T. Kiyosaki</h4>)}
 
 ReactDOM.render(<Booklist/>, document.getElementById("root") )
