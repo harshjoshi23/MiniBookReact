f3f20ddb50ed0994821d68490e4816c35f6ffc88
Harshvardhan Joshi <48888309+harshjoshi23@users.noreply.github.com>
2022-06-22 09:57:22 +0530

    Commits

--- src/index.js
+++ src/index.js
@@ -7,39 +7,67 @@ import "./index.css";
 //Creating array of objects
 const books = [
   {
+    id: 1,
     img: "https://m.media-amazon.com/images/I/81bsw6fnUiL._AC_UL480_QL65_.jpg",
     title: "Rich Dad Poor Dad:",
     author: "Robert T. Kiyosaki",
   },
   {
+    id: 2,
     img:
       "https://images-na.ssl-images-amazon.com/images/I/71GqMTCFWtL._AC._SR360,460.jpg",
     title: "Extreme Ownership: How U.S. Navy SEALs Lead ",
     author: "ocko Willink",
   },
   {
+    id: 3,
     img: "https://m.media-amazon.com/images/I/81zwIAF774L._AC_UL480_QL65_.jpg",
     title: "The War on the West",
     author: "Douglas Murray",
   },
 ];
+
 function Booklist() {
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book book={book}></Book>;
+        return <Book key={book.id} {...book}></Book>;
       })}
     </section>
   );
 }
-const Book = (props) => {
-  const { img, title, author } = props.book;
+const Book = ({ img, title, author }) => {
+  // const { img, title, author } = props.book;
+
+  // Refrence function
+  const clickHandler = () => {
+    alert("Hello World");
+  };
+
+  const complexExample = (author) => {
+    // Consol log the author each and every time we
+    // click on the button
+    // This is passed dynamically we are looking for author as an argument
+    console.log(author);
+  };
+
   return (
     <article className="book">
-      {/*Accessing the props */}
       <img src={img} alt="" />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
+
+      {/* clickHandleris the Function */}
+
+      <h1 type="button" onClick={clickHandler}>
+        {title}
+      </h1>
+
+      {/* Using an inline function  */}
+
+      <h4 onClick={() => console.log(author)}>{author}</h4>
+
+      <button type="button" onClick={complexExample(author)}>
+        Example for ref
+      </button>
     </article>
   );
 };
