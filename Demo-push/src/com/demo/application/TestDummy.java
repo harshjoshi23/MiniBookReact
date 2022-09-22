61ed9038aed2b63e6dfb4fc55bb4ab19086ca1d9
srivatsan <vatsan_k@hotmail.com>
2022-09-22 10:34:13 +0530

    Commit

---
+++ Demo-push/src/com/demo/application/TestDummy.java
@@ -0,0 +1,11 @@
+package com.demo.application;
+
+public class TestDummy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Somnath");
+
+	}
+
+}
